8a7454ffc8e415fa08c068b9c5044018e9e5f31c
Vladislav301 <gordeevvlad151@gmail.com>
2024-11-24 23:42:47 +0900

    Add files via upload

---
+++ Практическая 3 (1)/gru.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Практическая_3__1_
+{
+    public class gru
+    {
+        public void HelloWorld(string name)
+        {
+            Console.WriteLine($"Привет, {name}");
+        }
+
+        public void Surname(string surname)
+        {
+            Console.WriteLine($"{surname}");
+        }
+
+        public static DateTime? GetApproximateBirthDate(int age)
+        {
+            if (age <= 0)
+            {
+                return null;
+            }
+            DateTime today = DateTime.Today;
+            return today.AddYears(-age);
+        }
+
+
+
+    }
+    
+    
+}
---
+++ Практическая 3 (2)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Практическая_3__1_;
+
+namespace Практическая_3__2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            gru func = new gru();
+
+            func.HelloWorld("Дмитрий");
+            func.Surname("Павлов");
+            Console.Write("Введите возраст: ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+              {
+            DateTime? birthDate = gru.GetApproximateBirthDate(age);
+            if (birthDate.HasValue)
+              {
+            Console.WriteLine($"Приблизительная дата рождения: {birthDate.Value.ToShortDateString()}");
+              }
+            else
+              {
+            Console.WriteLine("Некорректный возраст.");
+              }
+              }
+             else
+              {
+            Console.WriteLine("Неверный формат ввода.");
+              }
+            Console.ReadLine();
+        }
+        
+    }
+    
+}
